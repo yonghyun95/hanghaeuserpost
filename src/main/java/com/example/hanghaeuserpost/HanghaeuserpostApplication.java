717ba2acc7e676cb627bee572c2aca5606a0c11a
Yonghyun Lee <dldydgus95426@gmail.com>
2023-03-23 13:42:53 +0900

    Initial commit

---
+++ src/main/java/com/example/hanghaeuserpost/HanghaeuserpostApplication.java
@@ -0,0 +1,13 @@
+package com.example.hanghaeuserpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeuserpostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanghaeuserpostApplication.class, args);
+    }
+
+}
